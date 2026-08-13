6f7e68ebf3a73e38bf4fada1c6a9c2c5b1d8738e
Sanjay N <sanjayn29.aiml@gmail.com>
2026-08-13 22:06:06 +0530

    variable  prog

---
+++ variable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a = 87;
+    char b = 'S';
+    float c = 8.26;
+    int A = 231;
+    char _B = 'V';
+
+    printf("%d\n,%d\n",a,A);
+    
+    return 0;
+}
